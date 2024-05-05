bc8fa14886fdbd4399899fb197d047755a671d26
tawanda-bbd <156799248+tawanda-bbd@users.noreply.github.com>
2024-05-05 22:58:45 +0200

    Side Bar

--- src/public/components/index.js
+++ src/public/components/index.js
@@ -1,3 +1,5 @@
 import SearchHeaderComponent from "./search-header-component/search-header-component.js";
+import SidebarComponent from "./side-bar-component/side-bar-component.js";
 
 export { SearchHeaderComponent };
+export { SidebarComponent };
