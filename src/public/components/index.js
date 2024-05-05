6d18e0e92a1d7c3c633219f658713fea43cbaf48
Verushan Naidoo <verushan.naidoo@bbd.co.za>
2024-05-05 12:25:28 +0200

    Added spider view component

--- src/public/components/index.js
+++ src/public/components/index.js
@@ -1,4 +1,5 @@
 import SearchHeaderComponent from "./search-header-component/search-header-component.js";
 import FileActionsComponent from "./file-actions-component/file-actions-component.js";
+import SpiderViewComponent from "./spider-view-component/spider-view-component.js";
 
-export { SearchHeaderComponent, FileActionsComponent };
+export { SearchHeaderComponent, FileActionsComponent, SpiderViewComponent };
