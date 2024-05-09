b5c41e15f0b5d14321f6cc6d7b8caae6675207b3
Batandwa Mgutsi <Batandwa.Mgutsi@bbd.co.za>
2024-05-09 16:35:24 +0200

    Do not attempt to call API when user is not logged in my spiders

--- src/public/pages/my-spiders-page/my-spiders-page.js
+++ src/public/pages/my-spiders-page/my-spiders-page.js
@@ -1,3 +1,5 @@
+import { getAccessToken } from "../../services/auth-service.js"
+
 export default class MySpidersPage extends HTMLElement {
   navigateBack(offset) {
     const spiderViewElement = this.shadowRoot.querySelector("spider-view");
@@ -15,31 +17,35 @@ export default class MySpidersPage extends HTMLElement {
   }
 
   connectedCallback() {
-    const shadow = this.attachShadow({ mode: "open" });
-    const template = document
-      .getElementById("my-spiders-page")
-      .content.cloneNode(true);
-    shadow.appendChild(template);
+    if (getAccessToken()) {
+      const shadow = this.attachShadow({ mode: "open" });
+      const template = document
+        .getElementById("my-spiders-page")
+        .content.cloneNode(true);
+      shadow.appendChild(template);
 
-    shadow.addEventListener("folderAdded", event => {
-      this.refreshFileView();
-      event.stopPropagation();
-    });
+      shadow.addEventListener("folderAdded", (event) => {
+        this.refreshFileView();
+        event.stopPropagation();
+      });
 
-    shadow.addEventListener("refresh", event => {
-      this.refreshFileView();
-      event.stopPropagation();
-    });
+      shadow.addEventListener("refresh", (event) => {
+        this.refreshFileView();
+        event.stopPropagation();
+      });
 
-    shadow.addEventListener("navigateBack", event => {
-      this.navigateBack(event.detail.offset);
-      event.stopPropagation();
-    });
+      shadow.addEventListener("navigateBack", (event) => {
+        this.navigateBack(event.detail.offset);
+        event.stopPropagation();
+      });
 
-    shadow.addEventListener("folderEntered", event => {
-      this.navigateForward(event.detail.folderName);
-      event.stopPropagation();
-    });
+      shadow.addEventListener("folderEntered", (event) => {
+        this.navigateForward(event.detail.folderName);
+        event.stopPropagation();
+      });
+    } else {
+      location.assign("/");
+    }
   }
 
   disconnectedCallback() {}
